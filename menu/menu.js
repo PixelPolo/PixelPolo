9ac7c700a8d771932153d71f613937c8dc564b88
PixelPolo <103859068+PixelPolo@users.noreply.github.com>
2024-01-29 18:38:48 +0100

    Delete menu directory

--- menu/menu.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const menuBtn = document.querySelector(".menuBtn");
-const menu = document.querySelector(".menu");
-const content = document.querySelectorAll(".content");
-menuBtn.addEventListener("click", () => {
-  content.forEach((e) => e.classList.toggle("blur"));
-  menu.classList.toggle("menuActive");
-});
